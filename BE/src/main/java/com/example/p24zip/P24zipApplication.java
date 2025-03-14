27ec179e3c18a9a9d58f7b66944c92b6420876f7
ywJung <sesac.yw@gmail.com>
2025-03-14 22:13:20 +0900

    [P24-11] chore: DB 도커 세팅 및 prettier 설정 (#5)
    * [P24-11] chore: DB 도커 세팅 및 prettier 설정

--- BE/src/main/java/com/example/p24zip/P24zipApplication.java
+++ BE/src/main/java/com/example/p24zip/P24zipApplication.java
@@ -2,7 +2,9 @@ package com.example.p24zip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class P24zipApplication {
 
