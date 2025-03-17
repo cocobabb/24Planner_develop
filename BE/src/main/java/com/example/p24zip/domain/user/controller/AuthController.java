26f8237bde40e517e28f87c4deb1c4afa8a06047
cocobabb <cocobab05108@gmail.com>
2025-03-17 11:39:52 +0900

    [P24-17] feat: [BE] 사용자 관련 Controller, Service 클래스명 변경 (#8)

--- BE/src/main/java/com/example/p24zip/domain/user/controller/AuthController.java
+++ BE/src/main/java/com/example/p24zip/domain/user/controller/AuthController.java
@@ -1,4 +1,30 @@
 package com.example.p24zip.domain.user.controller;
 
+import com.example.p24zip.domain.user.dto.request.SignupRequestDto;
+import com.example.p24zip.domain.user.service.AuthService;
+import com.example.p24zip.global.response.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
 public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<ApiResponse<Void>> signup(
+        @RequestBody @Valid SignupRequestDto requestDto) {
+        authService.signup(requestDto);
+
+        return ResponseEntity.ok(
+            ApiResponse.ok("CREATED", "회원가입을 성공했습니다.", null)
+        );
+    }
 }
--- BE/src/main/java/com/example/p24zip/domain/user/service/AuthService.java
+++ BE/src/main/java/com/example/p24zip/domain/user/service/AuthService.java
@@ -1,4 +1,39 @@
 package com.example.p24zip.domain.user.service;
 
+
+import com.example.p24zip.domain.user.dto.request.SignupRequestDto;
+import com.example.p24zip.domain.user.entity.User;
+import com.example.p24zip.domain.user.repository.UserRepository;
+import com.example.p24zip.global.exception.CustomException;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public void signup(@Valid SignupRequestDto requestDto) {
+        boolean checkUsername = checkExistsUsername(requestDto.getEmail());
+
+        if (checkUsername) {
+            throw new CustomException("EXIST_EMAIL", "이미 사용중인 이메일입니다.");
+        }
+
+        User user = requestDto.toEntity();
+        String encryptedPassword = passwordEncoder.encode(requestDto.getPassword());
+        user.setPassword(encryptedPassword);
+        userRepository.save(user);
+    }
+
+    public boolean checkExistsUsername(String userName) {
+        return userRepository.existsByUsername(userName);
+    }
 }
