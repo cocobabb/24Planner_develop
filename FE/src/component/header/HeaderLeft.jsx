067b1b4341e84fe9a71fd8472d95142ea8637daf
bjh0499 <j9603h@gmail.com>
2025-03-20 09:46:05 +0900

    [P24-42] feat: 헤더에 URL에 따른 구성 변화와 로그아웃 기능 추가 (#31)

--- FE/src/component/header/HeaderLeft.jsx
+++ FE/src/component/header/HeaderLeft.jsx
@@ -1,14 +1,18 @@
 import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import logo from '../../logo.png';
 
 export default function HeaderLeft() {
+  const location = useLocation();
+  const splitedUrlString = location.pathname.split('/');
+
+  // Linux 환경 Firefox에서 flex에 이미지가 들어가면 무조건 크기를 최대로 차지하는 문제가 있어 flex 크기 수동 지정
   const headerListStyle = 'flex flex-4 font-bold';
   const headerItemStyle = 'flex items-center p-4';
   const headerLogoStyle = headerItemStyle + ' max-w-40';
   const headerActiveItemStyle = headerItemStyle + ' text-primary';
 
-  // Linux 환경 Firefox에서 flex에 이미지가 들어가면 무조건 크기를 최대로 차지하는 문제가 있어 flex 크기 수동 지정
-  // 주석 처리 부분은 로그인 후 화면까지 구현한 시점에서 활성화 예정
+  // NavLink로는 구분이 어려운 URL이 있어서 Link 상에서 Active 수동 구현
   return (
     <ul className={headerListStyle}>
       <li className={headerLogoStyle}>
@@ -17,10 +21,27 @@ export default function HeaderLeft() {
         </Link>
       </li>
 
-      {/*
-      <li className={headerActiveItemStyle}>체크리스트</li>
-      <li className={headerItemStyle}>캘린더</li>
-      */}
+      {splitedUrlString[1] === 'plans' &&
+      splitedUrlString[2] &&
+      Number.parseInt(splitedUrlString[2]) > 0 ? (
+        <>
+          <Link
+            to={`/${splitedUrlString[1]}/${splitedUrlString[2]}`}
+            className={splitedUrlString[3] === 'calendar' ? headerItemStyle : headerActiveItemStyle}
+          >
+            체크리스트
+          </Link>
+
+          <Link
+            to={`/${splitedUrlString[1]}/${splitedUrlString[2]}/calendar`}
+            className={splitedUrlString[3] === 'calendar' ? headerActiveItemStyle : headerItemStyle}
+          >
+            캘린더
+          </Link>
+        </>
+      ) : (
+        <></>
+      )}
     </ul>
   );
 }
--- FE/src/component/header/HeaderRight.jsx
+++ FE/src/component/header/HeaderRight.jsx
@@ -1,31 +1,62 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { logout } from '../../store/slices/authSlice';
 
 export default function HeaderRight() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const splitedUrlString = location.pathname.split('/');
+
+  const handleLogoutClick = () => {
+    dispatch(logout());
+    navigate('/');
+  };
+
   const headerListStyle = 'flex flex-6 justify-end';
   const headerItemStyle = 'flex items-center p-4';
   const headerDropdownStyle = headerItemStyle + ' relative group';
   const headerDropdownButtonStyle = 'text-secondary cursor-pointer';
   const headerDropdownBodyStyle =
-    'absolute text-xl text-center px-6 top-15 space-y-4 left-0 right-0 w-full py-4 bg-gray-100 shadow-sm opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300';
+    'absolute text-xl text-center top-15 space-y-4 left-0 right-0 w-full py-4 bg-gray-100 shadow-sm opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300';
+  const headerDropdownItemStyle = 'w-full';
+  const headerDropdownLinkStyle = 'block w-full';
 
-  // 주석 처리 부분은 로그인 후 화면까지 구현한 시점에서 활성화 예정
   // dropdown 구현 참고: https://medium.com/internet-of-technology/in-2-minutes-how-to-make-a-tailwind-css-dropdown-menu-817320bb0678
   return (
     <ul className={headerListStyle}>
-      {/*
-      <li className={headerDropdownStyle}>
-        <span className={headerDropdownButtonStyle}>이사 플랜 1</span>
-        <ul className={headerDropdownBodyStyle}>
-          <li>
-            <Link to="/plans">이사 목록</Link>
-          </li>
-          <li>
-            <Link to="/config">이사 설정</Link>
-          </li>
-        </ul>
-      </li>
-      <li className={headerItemStyle}>로그아웃</li>
-      */}
+      {splitedUrlString[1] === 'plans' &&
+      splitedUrlString[2] &&
+      Number.parseInt(splitedUrlString[2]) > 0 ? (
+        <li className={headerDropdownStyle}>
+          <span className={headerDropdownButtonStyle}>이사 플랜 이름</span>
+          <ul className={headerDropdownBodyStyle}>
+            <li className={headerDropdownItemStyle}>
+              <Link to="/plans" className={headerDropdownLinkStyle}>
+                이사 목록
+              </Link>
+            </li>
+            <li className={headerDropdownItemStyle}>
+              <Link to="/config" className={headerDropdownLinkStyle}>
+                이사 설정
+              </Link>
+            </li>
+          </ul>
+        </li>
+      ) : (
+        <></>
+      )}
+
+      {splitedUrlString[1] === 'plans' ? (
+        <li className={headerItemStyle}>
+          <div className="cursor-pointer" onClick={handleLogoutClick}>
+            로그아웃
+          </div>
+        </li>
+      ) : (
+        <></>
+      )}
     </ul>
   );
 }
