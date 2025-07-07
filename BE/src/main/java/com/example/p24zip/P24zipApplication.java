5e926a9bd2d0bcff5a3f7aca77b3f35aa50ed1a0
cocobabb <cocobab05108@gmail.com>
2025-07-08 02:15:42 +0900

    [fix] 데이터 저장 시간 타임존 에러 해결

--- BE/src/main/java/com/example/p24zip/P24zipApplication.java
+++ BE/src/main/java/com/example/p24zip/P24zipApplication.java
@@ -1,20 +1,19 @@
 package com.example.p24zip;
 
-import java.util.TimeZone;
+import com.example.p24zip.domain.movingPlan.service.NotificationService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import com.example.p24zip.domain.movingPlan.service.NotificationService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class P24zipApplication {
 
     public static void main(String[] args) {
-        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+//        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(P24zipApplication.class, args);
     }
 
