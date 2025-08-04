673e8dfaedb9f60b99153d36c4494e7ed49c8af0
cocobabb <cocobab05108@gmail.com>
2025-08-04 16:30:28 +0900

    [chore] NotificationService 경로 수정

--- BE/src/main/java/com/example/p24zip/global/notification/NotificationService.java
+++ BE/src/main/java/com/example/p24zip/global/notification/NotificationService.java
@@ -1,8 +1,7 @@
-package com.example.p24zip.domain.movingPlan.service;
+package com.example.p24zip.global.notification;
 
 import com.example.p24zip.domain.movingPlan.dto.response.HousemateNotificationDto;
 import com.example.p24zip.domain.user.entity.User;
-import com.example.p24zip.global.notification.NotificationResponseDto;
 import com.example.p24zip.global.redis.RedisNotificationDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
