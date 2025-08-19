d1a6663ee889e85c90bd23ba987034bf5b9df586
cocobabb <cocobab05108@gmail.com>
2025-08-20 01:41:02 +0900

    [fix] deploy 문제 해결

--- BE/src/main/java/com/example/p24zip/global/config/GoogleCredentialsConfig.java
+++ BE/src/main/java/com/example/p24zip/global/config/GoogleCredentialsConfig.java
@@ -2,11 +2,14 @@ package com.example.p24zip.global.config;
 
 
 import com.google.auth.oauth2.GoogleCredentials;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Base64;
 import java.util.Collections;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 public class GoogleCredentialsConfig {
@@ -15,12 +18,14 @@ public class GoogleCredentialsConfig {
      * Firebase Admin SDK(json file)의 비공개 키를 참조하여 Firebase와의 Authentication & Authorization
      **/
     @Bean
-    public GoogleCredentials googleCredentials() throws IOException {
-        return GoogleCredentials
-            .fromStream(
-                new ClassPathResource("firebase/firebase_service_key.json").getInputStream())
-            .createScoped(
-                Collections.singletonList("https://www.googleapis.com/auth/cloud-platform"));
+    public GoogleCredentials googleCredentials(
+        @Value("${FIREBASE_SERVICE_JSON}") String firebaseKeyBase64) throws IOException {
+        byte[] decoded = Base64.getDecoder().decode(firebaseKeyBase64);
+        try (InputStream in = new ByteArrayInputStream(decoded)) {
+            return GoogleCredentials.fromStream(in)
+                .createScoped(
+                    Collections.singletonList("https://www.googleapis.com/auth/cloud-platform"));
+        }
     }
 
 }
